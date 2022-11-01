1d093019cfcfc09920f1d38004937c0b85d4b969
Nils Martel <nilsmartel@yahoo.de>
2022-11-01 15:19:25 +0100

    Improve CLI Interface

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Replaces all occurences of a substring in a file
     replace-all 0.1.0
 
     USAGE:
-        replace-all <file> <word> <replacement>
+        replace-all <word> <replacement> <files...>
 
     FLAGS:
         -h, --help       Prints help information
         -V, --version    Prints version information
 
     ARGS:
-        <file>           Input file to use
+        <files>          One or more input files to use
         <word>           String to find and replace
         <replacement>    Replacement
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Config {
-    /// Input file to use
-    #[structopt()]
-    file: String,
-
     /// String to find and replace
     #[structopt()]
     word: String,
@@ -15,26 +11,32 @@ struct Config {
     /// Replacement
     #[structopt()]
     replacement: String,
+
+    /// One or more input files to use
+    #[structopt()]
+    files: Vec<String>,
 }
 
 fn main() {
     let Config {
-        file,
+        files,
         word,
         replacement,
     } = Config::from_args();
 
     // open file
-    let content = std::fs::read_to_string(&file).expect("to read file");
+    for file in files {
+        let content = std::fs::read_to_string(&file).expect("to read file");
 
-    if !content.contains(&replacement) {
-        eprintln!("no occurence found in file {}", file);
-        std::process::exit(0);
+        if !content.contains(&replacement) {
+            eprintln!("no occurence found in file {}", file);
+            continue;
+        }
+
+        let content = content.replace(&word, &replacement);
+
+        let mut file = std::fs::File::create(file).expect("overwrite file");
+
+        write!(&mut file, "{}", content).expect("write content to file");
     }
-
-    let content = content.replace(&word, &replacement);
-
-    let mut file = std::fs::File::create(file).expect("overwrite file");
-
-    write!(&mut file, "{}", content).expect("write content to file");
 }
